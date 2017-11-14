ad1d436d62fb1f62fbaa289b627458833bd9e08a
KenyettaMichelle <31255707+KenyettaMichelle@users.noreply.github.com>
2017-11-14 15:19:05 -0500

    Button and mouse
    mouse moves and song plays

--- sketch.js
+++ sketch.js
@@ -1,76 +1,43 @@
-var posX = 0;
-var posY = 0;
-var directionY = false;
+var interfaceItems = [];
 
-var startButton;
-var startBounce = false;
+var brushSize = 10;
+var song;
+var button;
+function setup(){
+  createCanvas(400,400);
 
+  interfaceItems.push(new interface(100,100,50, color(255,0,0)));
 
+  interfaceItems.push(new interface(200,100,50, color(0,255,0)));
+song = load sound("rainbow.mp3", loaded);
+    button = createButton("play");
 
-function setup() {
-    
-    createCanvas(400, 400);
-        
-    
-startButton = createButton("start");
-startButton.position(0, 400);
-startButton.mousePressed(function(){ 
-    posY = 0;
-    if (startBounce == false) {
-        startBounce = true;
-    }else{
-        startBounce = false;
-    }
-});
-    
-    
-    
 }
 
-function draw() {
-    
-frameRate (60);
-background('red');
-    
-    if (startBounce == true){
-        
-    ellipse(width / 2, posY, 50, 50);
-    
-    if (directionY == false){
-        posY++;
-    } else {
-        posY--;
-    
-    } 
-        if (posY >=height) {
-            directionY = true;
-        }
-    
-    if (posY <=0){
-        directionY = false;
-       }
-        
-    }    
-    
-    fill('orange');
-    stroke("cyan");
-    strokeWeight(4);
-    
-    
-        
-    ellipse(127,127,100,100);
-    
-    fill('purple');
-    ellipse(110,105,10,10);
-    
-    fill('purple');
-    ellipse(140,105,10,10)
 
-    fill('orange');
-    arc(125,125, 15, 5, HALF_PI, PI);
-    
-    line(100,150,150,150)
-       
-    
+function draw(){
+  fill(0);
+  ellipse(mouseX,mouseY, brushSize,brushSize);
+
+  interfaceItems[0].check();
+  interfaceItems[0].display();
+  interfaceItems[1].check();
+  interfaceItems[1].display();
+  //console.log(interfaceItems[0].check());
 }
 
+function mousePressed(){
+  if(interfaceItems[0].check() == true){
+    console.log("pressed red button");
+    brushSize++;
+  }
+
+  if(interfaceItems[1].check() == true){
+    brushSize--;
+  }
+function togglePlaying(){
+    song.play();
+    song.setVolume(0.3);
+}
+
+}
